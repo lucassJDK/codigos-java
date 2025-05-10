ce12492c6e61337d01ab36665af72d9f54e776b7
lucassJDK <lucasslkjjj@gmail.com>
2025-05-10 15:23:43 +0000

    Arquivo main finalizado

--- java orientado a objetos/fibonnaci/src/Main.java
+++ java orientado a objetos/fibonnaci/src/Main.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Insira até que termo da sequêcia você quer imprimir:");
+        Scanner scan = new Scanner(System.in);
+        int nTerm = scan.nextInt();
+        Fibonnaci.generateSequenceInRange(nTerm);
+    }
 }
