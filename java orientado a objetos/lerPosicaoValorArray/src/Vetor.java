22c4d0017dbbd868e44ab2bc202d1946393466da
lucassJDK <lucasslkjjj@gmail.com>
2025-05-10 18:43:46 +0000

    logica da classe Vetor finalizada

---
+++ java orientado a objetos/lerPosicaoValorArray/src/Vetor.java
@@ -0,0 +1,8 @@
+public class Vetor {
+    public static void lerLinhaEValor(int [] numbers) {
+        int cout = 0;
+        for(int number : numbers) {
+            System.out.printf("na posição %d tem o valor %d \n",cout,number);
+        }
+    }
+}
