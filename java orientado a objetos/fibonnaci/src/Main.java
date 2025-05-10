8ad8131432d66815e2e57f6703ef070ce08a4367
lucassJDK <lucasslkjjj@gmail.com>
2025-05-10 15:51:56 +0000

    adicionando tratamento de exceções na Main e finalizando exerccioí

--- java orientado a objetos/fibonnaci/src/Main.java
+++ java orientado a objetos/fibonnaci/src/Main.java
@@ -5,6 +5,22 @@ public class Main {
         System.out.println("Insira até que termo da sequêcia você quer imprimir:");
         Scanner scan = new Scanner(System.in);
         int nTerm = scan.nextInt();
-        Fibonnaci.generateSequenceInRange(nTerm);
+        try {
+            Fibonnaci.generateSequenceInRange(nTerm);
+        } catch(ArrayIndexOutOfBoundsException e) {
+              switch(nTerm) {
+                  case 0:
+                      System.out.println("insira um núero diferente de 0!");
+                      break;
+                  case 1:
+                      System.out.println("1");
+                      break;
+                  case 2:
+                      System.out.println("1,1");
+                      break;
+                  default:
+                      System.out.println("um erro inesperado aconteceu,relate o erro e tente novamente mais tarde.");
+              }
+              }
+        }
     }
-}
