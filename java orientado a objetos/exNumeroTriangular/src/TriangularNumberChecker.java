f055d4dab7c2f4041c368db8823ac1702def905d
lucassJDK <lucasslkjjj@gmail.com>
2025-05-10 12:38:24 +0000

    subindo atividade de checar número triangular

--- java orientado a objetos/exNumeroTriangular/src/TriangularNumberChecker.java
+++ java orientado a objetos/exNumeroTriangular/src/TriangularNumberChecker.java
@@ -6,7 +6,7 @@ public class TriangularNumberChecker {
         while (true) {
             result = cout * (cout + 1) * (cout + 2);
             if (userNumber == result) {
-                System.out.printf("O número %d é um número triangular!pois %d.%d.%d = %d", userNumber, cout, cout + 1, cout + 2, userNumber);
+                System.out.printf("O número %d é um número triangular! pois %d.%d.%d = %d", userNumber, cout, cout + 1, cout + 2, userNumber);
                 return;
             } else if (result > userNumber) {
                    System.out.println("o número "+userNumber +" não é triangular!");
